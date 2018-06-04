1e5e914361a920ef5138d398dbe40158dcd079ee
Enesto Freyre <ernestofreyreg@gmail.com>
2018-06-04 14:44:50 -0700

    Added rollup

---
+++ build/ui-components.js
@@ -0,0 +1,105 @@
+import { createElement, Component } from 'react';
+
+function styleInject(css, ref) {
+  if ( ref === void 0 ) ref = {};
+  var insertAt = ref.insertAt;
+
+  if (!css || typeof document === 'undefined') { return; }
+
+  var head = document.head || document.getElementsByTagName('head')[0];
+  var style = document.createElement('style');
+  style.type = 'text/css';
+
+  if (insertAt === 'top') {
+    if (head.firstChild) {
+      head.insertBefore(style, head.firstChild);
+    } else {
+      head.appendChild(style);
+    }
+  } else {
+    head.appendChild(style);
+  }
+
+  if (style.styleSheet) {
+    style.styleSheet.cssText = css;
+  } else {
+    style.appendChild(document.createTextNode(css));
+  }
+}
+
+var css = ".Button_Button__3Tk67 {\n  background-color: #b0eb00;\n  color: white;\n  padding: 4px 10px;\n  border-radius: 8px;\n  border: none;\n  outline: none;\n  cursor: pointer; }\n";
+var styles = { "Button": "Button_Button__3Tk67" };
+styleInject(css);
+
+var classCallCheck = function (instance, Constructor) {
+  if (!(instance instanceof Constructor)) {
+    throw new TypeError("Cannot call a class as a function");
+  }
+};
+
+var createClass = function () {
+  function defineProperties(target, props) {
+    for (var i = 0; i < props.length; i++) {
+      var descriptor = props[i];
+      descriptor.enumerable = descriptor.enumerable || false;
+      descriptor.configurable = true;
+      if ("value" in descriptor) descriptor.writable = true;
+      Object.defineProperty(target, descriptor.key, descriptor);
+    }
+  }
+
+  return function (Constructor, protoProps, staticProps) {
+    if (protoProps) defineProperties(Constructor.prototype, protoProps);
+    if (staticProps) defineProperties(Constructor, staticProps);
+    return Constructor;
+  };
+}();
+
+var inherits = function (subClass, superClass) {
+  if (typeof superClass !== "function" && superClass !== null) {
+    throw new TypeError("Super expression must either be null or a function, not " + typeof superClass);
+  }
+
+  subClass.prototype = Object.create(superClass && superClass.prototype, {
+    constructor: {
+      value: subClass,
+      enumerable: false,
+      writable: true,
+      configurable: true
+    }
+  });
+  if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass;
+};
+
+var possibleConstructorReturn = function (self, call) {
+  if (!self) {
+    throw new ReferenceError("this hasn't been initialised - super() hasn't been called");
+  }
+
+  return call && (typeof call === "object" || typeof call === "function") ? call : self;
+};
+
+var Button = function (_React$Component) {
+  inherits(Button, _React$Component);
+
+  function Button() {
+    classCallCheck(this, Button);
+    return possibleConstructorReturn(this, (Button.__proto__ || Object.getPrototypeOf(Button)).apply(this, arguments));
+  }
+
+  createClass(Button, [{
+    key: 'render',
+    value: function render() {
+      return createElement(
+        'button',
+        {
+          className: styles.Button,
+          onClick: this.props.onClick
+        },
+        this.props.children
+      );
+    }
+  }]);
+  return Button;
+}(Component);
+//# sourceMappingURL=ui-components.js.map
---
+++ rollup.config.babel.js
@@ -0,0 +1,36 @@
+import babel from 'rollup-plugin-babel'
+import resolve from 'rollup-plugin-node-resolve'
+import commonjs from 'rollup-plugin-commonjs'
+import replace from 'rollup-plugin-replace'
+import postcss from 'rollup-plugin-postcss'
+
+export default {
+  input: './components/index.js',
+
+  output: {
+    file: './build/ui-components.js',
+    format: 'es',
+    name: 'UIComponents',
+    sourcemap: true,
+    globals: {
+      react: 'React',
+      'react-dom': 'ReactDOM'
+    }
+  },
+
+  plugins: [
+    postcss({
+      modules: true
+    }),
+    babel({
+      exclude: 'node_modules/**'
+    }),
+    replace({
+      'process.env.NODE_ENV': JSON.stringify('development')
+    }),
+    resolve(),
+    commonjs()
+  ],
+
+  external: ['react', 'react-dom']
+}
